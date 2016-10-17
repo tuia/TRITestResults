ae769871a824d3cb1673b1187da66893b5bb3553
Nathália Xavier <nathaliazxavier@gmail.com>
2016-10-17 13:13:48 -0200

    Commit Inicial

---
+++ js/charts.js
@@ -0,0 +1,74 @@
+//TEMA
+Highcharts.theme = {
+    colors: ['#31708F', '#3C763D', '#A94442', '#8A6D3B'],
+    chart: {
+        marginTop: 60
+    },
+    title: {
+        style: {
+            color: '#333333',
+            font: '400 18px "Roboto", sans-serif;'
+        }
+    },
+}
+Highcharts.setOptions(Highcharts.theme);
+
+
+    // LINE CHART
+    $(function () {
+
+        var options = {
+            data: {
+                table: document.getElementById('linechart-table'),
+            },
+            chart: {
+                type: 'line',
+                marginTop: 80,
+            },
+            title: {
+                text: 'Alternativa Escolhida por grupo de respondentes',
+                align: 'left',
+            },
+            yAxis: {
+                allowDecimals: true,
+                title: {
+                    text: 'Units'
+                }
+            }
+        };
+
+        $('#line-chart').highcharts(Highcharts.merge(options));
+    });
+
+
+    // BAR CHART
+    $(function () {
+
+        var options = {
+            data: {
+                table: document.getElementById('barchart-table'),
+                endColumn: 1,
+            },
+            chart: {
+                type: 'column',
+                marginTop: 80,
+            },
+            yAxis: {
+                allowDecimals: true,
+                title: {
+                    text: 'Proporção de respondentes'
+                }
+            },
+            xAxis: {
+                title: {
+                    text: 'Alternativas escolhidas'
+                }
+            },
+            title: {
+                text: 'Porcentagem de respondentes que escolheram cada alternativa',
+                align: 'left',
+            }
+        };
+
+        $('#bar-chart').highcharts(Highcharts.merge(options));
+    });
